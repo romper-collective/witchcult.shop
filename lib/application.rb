b73185c7265644f0637903e994e5304ef2cf5e40
tamsin johnson <tomjohnson@ucsb.edu>
2020-06-07 16:25:10 -0700

    work on adding a database connection for list items
    We're pretty sure we need items on our list, and for them to persist, so we're
    working on that first.

--- lib/application.rb
+++ lib/application.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
+require 'sequel'
+Dir.glob("#{settings.root}/models/**/*").each { |path| require(path) }
 
 class Application < Sinatra::Base
+  # DB = Sequel.sqlite
+
   get '/healthz' do
     'Ok!'
   end
