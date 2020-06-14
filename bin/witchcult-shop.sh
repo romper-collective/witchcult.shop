09d617f477ca64b0d954d42fe3be70ac915f5486
tamsin johnson <tomjohnson@ucsb.edu>
2020-06-14 11:12:38 -0700

    Wait for postgres on app startup; be more verbose

--- bin/witchcult-shop.sh
+++ bin/witchcult-shop.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+
+echo "Starting witchcult.shop app"
+while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"
+do
+  echo "Waiting for postgresql"
+  sleep 1
+done
+
+echo "Connecting to postgresql"
 bundle exec sequel -m ./migrations postgres://psql:postgres-devel@postgres:5432/witchcult-shops
 bundle exec rackup -o 0.0.0.0 -p 9000
