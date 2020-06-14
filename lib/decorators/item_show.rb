c24984bfa2a5c0112c77fdb8ee33acaa80520b85
tamsin johnson <tomjohnson@ucsb.edu>
2020-06-14 16:30:33 -0700

    decorate list items; add slug as `<li id={slug}>`
    adds an id to list items. to get this we add a decorator for list items, and
    extend the current `ListShow` decorator to wrap returned items.

--- lib/decorators/item_show.rb
+++ lib/decorators/item_show.rb
@@ -1,24 +1,16 @@
 # frozen_string_literal: true
 
 ##
-# Decorate `List` for display in list views.
+# Decorate `ListItem` for display in list views.
 #
 # @see https://ruby-doc.org/stdlib-2.7.1/libdoc/delegate/rdoc/Delegator.html
-class ListShow < SimpleDelegator
-  ##
-  # @return [Enumerable<ItemShow>]
-  def list_items
-    super.map do |item|
-      ItemShow.new(item)
-    end
-  end
-
+class ItemShow < SimpleDelegator
   ##
   # Wraps the parent model's name, with a guaranteed return type.
   #
   # @return [String]
   def name
-    super || 'Unnamed List'
+    super || 'Unknown Item'
   end
 
   ##
--- spec/decorators/list_show_spec.rb
+++ spec/decorators/list_show_spec.rb
@@ -6,6 +6,25 @@ RSpec.describe ListShow do
   subject(:decorator) { described_class.new(model) }
   let(:model) { List.new }
 
+  describe '#list_items' do
+    it { is_expected.to have_attributes(list_items: be_empty) }
+
+    context 'with some list items' do
+      let(:model) { List.create }
+
+      before do
+        3.times { |i| model.add_list_item(name: "item #{i}") }
+      end
+
+      it 'decorates list items' do
+        expect(decorator.list_items)
+          .to contain_exactly(have_attributes(slug: 'item_0'),
+                              have_attributes(slug: 'item_1'),
+                              have_attributes(slug: 'item_2'))
+      end
+    end
+  end
+
   describe '#name' do
     it 'returns a default name' do
       expect(decorator.name).to be_a String
