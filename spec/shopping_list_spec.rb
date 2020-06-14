207d9c6b254a0da35be35c995db766b32af496cd
Kara Woo <woo.kara@gmail.com>
2020-06-14 13:00:07 -0700

    Change test to look for only the things in the current shopping list
    We don't want every single list item to be included, only the ones that are
    actually on the list we care about.

--- spec/shopping_list_spec.rb
+++ spec/shopping_list_spec.rb
@@ -22,13 +22,20 @@ RSpec.describe 'GET /shopping_list' do
   end
 
   context 'with some list items' do
+    let(:list) { List.create(name: 'Shopping List') }
     let(:list_item_names) { ['cheese sticks', 'bubbles'] }
 
-    it 'gives a list of the items' do
-      list_item_names.each { |name| ListItem.create(name: name) }
+    before do
+      list_item_names.each do |name|
+        list.list_items << ListItem.create(name: name)
+      end
+      ListItem.create(name: 'not in shopping list')
+    end
 
+    it 'gives a list of the items' do
       get '/shopping_list'
       expect(last_response.body).to include('cheese sticks', 'bubbles')
+      expect(last_response.body).not_to include('not in shopping list')
     end
   end
 end
