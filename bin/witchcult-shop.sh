0e013a5d7b600f6d3e32a64c2984c90f5f5ec141
tamsin johnson <tomjohnson@ucsb.edu>
2020-06-20 12:43:08 -0700

    make docker entrypoint sensitive to environment variables
    we hardcoded the postgres connection details, but these will vary across
    environments. use the existing environment variables instead.

--- bin/witchcult-shop.sh
+++ bin/witchcult-shop.sh
@@ -8,5 +8,5 @@ do
 done
 
 echo "Connecting to postgresql"
-bundle exec sequel -m ./migrations postgres://psql:postgres-devel@postgres:5432/witchcult-shops
+bundle exec sequel -m ./migrations postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB
 bundle exec rackup -o 0.0.0.0 -p 9000
